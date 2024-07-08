c3d17f5e9a31f95f883b5369a9fc6fc12c3ce484
AyobamiAjibola <ayobamiajibola46@gmail.com>
2024-07-08 23:47:20 +0100

    .

--- client/src/app/components/Footer.tsx
+++ client/src/app/components/Footer.tsx
@@ -114,171 +114,173 @@ export default function Footer() {
     },[]);
       
     return (
-        <Box
-            sx={{
-                width: '100%',
-                backgroundColor: theme.palette.secondary.lightest,
-                px: isMobile ? '20px' : '90px',
-                py: isMobile ? 4 : 5,
-                borderTop: `1px solid ${theme.palette.secondary.lighter}`,
-                display: 'flex',
-                flexDirection: 'column'
-            }}
-        >
-            <Box
-                sx={{
-                    display: 'flex',
-                    flexDirection: isMobile ? 'column' : 'row',
-                    justifyContent: isMobile ? 'flex-start' : 'space-between',
-                    alignItems: isMobile ? 'flex-start' : 'center',
-                    gap: isMobile ? 2 : 0
-                }}
-            >
-                <Image
-                    src='/ipatient-logo.png'
-                    alt='logo'
-                    height={100}
-                    width={100}
-                />
-                <Box display={'flex'} gap={3}>
-                    <Box
-                        sx={{
-                            display: 'flex',
-                            gap: 3
-                        }}
-                    >
-                        {
-                            links.slice(0, 4).map((link, index: number) => (
-                                <Typography key={index}
-                                    onClick={() => {
-                                        if(link.name === 'Resources') return null;
-                                        router.push(`${link.link}`)}}
-                                    sx={{
-                                        color: theme.palette.primary.darker,
-                                        fontSize: isMobile ? theme.typography.labelxs.fontSize : theme.typography.labelsm.fontSize,
-                                        fontWeight: theme.typography.labelsm.fontWeight,
-                                        cursor: 'pointer',
-                                        '&:hover': {
-                                            color: theme.palette.primary.main
-                                        }
-                                    }}
-                                >
-                                    { link.name }
-                                </Typography>
-                            ))
-                        }
-                    </Box>
-                    <Box
-                        sx={{
-                            display: 'flex',
-                            flexDirection: 'column',
-                            gap: 2
-                        }}
-                    >
-                        <Dropdown menu={{ items }} placement="bottomRight" arrow={{ pointAtCenter: true }} trigger={['click']}>
-                            <Typography sx={{cursor: 'pointer'}}
-                                onClick={(e) => e.preventDefault()}
-                                variant={isMobile ? 'labelxs' : 'labelsm'}
-                                color={theme.palette.primary.darker}
-                            >
-                                Resources
-                            </Typography>
-                        </Dropdown>
-                    </Box>
-                </Box>
-                <Box
-                    sx={{
-                        display: 'flex',
-                        ml: isMobile ? -2 : 0
-                    }}
-                >
-                    <IconButton>
-                        <FacebookRounded/>
-                    </IconButton>
-                    <IconButton>
-                        <LinkedIn/>
-                    </IconButton>
-                    <IconButton>
-                        <Instagram/>
-                    </IconButton>
-                </Box>
-            </Box>
-
+        <>
             <Box
                 sx={{
                     width: '100%',
-                    height: '1px',
-                    backgroundColor: theme.palette.secondary.lighter,
-                    my: 4
-                }}
-            />
-
-            <Box
-                sx={{
+                    backgroundColor: theme.palette.secondary.lightest,
+                    px: isMobile ? '20px' : '90px',
+                    py: isMobile ? 4 : 5,
+                    borderTop: `1px solid ${theme.palette.secondary.lighter}`,
                     display: 'flex',
-                    flexDirection: isMobile ? 'column' : 'row',
-                    justifyContent: isMobile ? 'flex-start' : 'space-between',
-                    alignItems: isMobile ? 'flex-start' : 'center',
-                    gap: isMobile ? 2 : 0
+                    flexDirection: 'column'
                 }}
             >
-                <Typography
-                    sx={{
-                        fontSize: theme.typography.labelxs.fontSize,
-                        color: theme.palette.secondary.light
-                    }}
-                >
-                    <Copyright sx={{fontSize: '12px', mb: '2px'}}/> {new Date().getFullYear()} Patient.ng. All rights reserved.
-                </Typography>
                 <Box
                     sx={{
                         display: 'flex',
-                        alignItems: 'center', gap: 2
+                        flexDirection: isMobile ? 'column' : 'row',
+                        justifyContent: isMobile ? 'flex-start' : 'space-between',
+                        alignItems: isMobile ? 'flex-start' : 'center',
+                        gap: isMobile ? 2 : 0
                     }}
                 >
-                    <Typography onClick={()=>router.push('/privacy-policy')}
+                    <Image
+                        src='/ipatient-logo.png'
+                        alt='logo'
+                        height={100}
+                        width={100}
+                    />
+                    <Box display={'flex'} gap={3}>
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                gap: 3
+                            }}
+                        >
+                            {
+                                links.slice(0, 4).map((link, index: number) => (
+                                    <Typography key={index}
+                                        onClick={() => {
+                                            if(link.name === 'Resources') return null;
+                                            router.push(`${link.link}`)}}
+                                        sx={{
+                                            color: theme.palette.primary.darker,
+                                            fontSize: isMobile ? theme.typography.labelxs.fontSize : theme.typography.labelsm.fontSize,
+                                            fontWeight: theme.typography.labelsm.fontWeight,
+                                            cursor: 'pointer',
+                                            '&:hover': {
+                                                color: theme.palette.primary.main
+                                            }
+                                        }}
+                                    >
+                                        { link.name }
+                                    </Typography>
+                                ))
+                            }
+                        </Box>
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                gap: 2
+                            }}
+                        >
+                            <Dropdown menu={{ items }} placement="bottomRight" arrow={{ pointAtCenter: true }} trigger={['click']}>
+                                <Typography sx={{cursor: 'pointer'}}
+                                    onClick={(e) => e.preventDefault()}
+                                    variant={isMobile ? 'labelxs' : 'labelsm'}
+                                    color={theme.palette.primary.darker}
+                                >
+                                    Resources
+                                </Typography>
+                            </Dropdown>
+                        </Box>
+                    </Box>
+                    <Box
                         sx={{
-                            fontSize: theme.typography.labelxs.fontSize,
-                            color: theme.palette.primary.main,
-                            cursor: 'pointer'
+                            display: 'flex',
+                            ml: isMobile ? -2 : 0
                         }}
                     >
-                        Terms & Conditions
-                    </Typography>
-                    <FiberManualRecord sx={{color: theme.palette.secondary.light, fontSize: '10px'}}/>
-                    <Typography onClick={()=>router.push('/privacy-policy')}
-                        sx={{
-                            fontSize: theme.typography.labelxs.fontSize,
-                            color: theme.palette.primary.main,
-                            cursor: 'pointer'
-                        }}
-                    >
-                        Privacy policy
-                    </Typography>
-                    <FiberManualRecord sx={{color: theme.palette.secondary.light, fontSize: '10px'}}/>
+                        <IconButton>
+                            <FacebookRounded/>
+                        </IconButton>
+                        <IconButton>
+                            <LinkedIn/>
+                        </IconButton>
+                        <IconButton>
+                            <Instagram/>
+                        </IconButton>
+                    </Box>
+                </Box>
+
+                <Box
+                    sx={{
+                        width: '100%',
+                        height: '1px',
+                        backgroundColor: theme.palette.secondary.lighter,
+                        my: 4
+                    }}
+                />
+
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: isMobile ? 'column' : 'row',
+                        justifyContent: isMobile ? 'flex-start' : 'space-between',
+                        alignItems: isMobile ? 'flex-start' : 'center',
+                        gap: isMobile ? 2 : 0
+                    }}
+                >
                     <Typography
-                        onMouseEnter={()=>setType('about')}
-                        onClick={()=>setOpen(true)}
                         sx={{
                             fontSize: theme.typography.labelxs.fontSize,
-                            color: theme.palette.primary.main,
-                            cursor: 'pointer'
+                            color: theme.palette.secondary.light
                         }}
                     >
-                        About us
+                        <Copyright sx={{fontSize: '12px', mb: '2px'}}/> {new Date().getFullYear()} Patient.ng. All rights reserved.
                     </Typography>
-                    <FiberManualRecord sx={{color: theme.palette.secondary.light, fontSize: '10px'}}/>
-                    <Typography
-                        onMouseEnter={()=>setType('contact')}
-                        onClick={()=>setOpen(true)}
+                    <Box
                         sx={{
-                            fontSize: theme.typography.labelxs.fontSize,
-                            color: theme.palette.primary.main,
-                            cursor: 'pointer'
+                            display: 'flex',
+                            alignItems: 'center', gap: 2
                         }}
                     >
-                        Contact us
-                    </Typography>
+                        <Typography onClick={()=>router.push('/privacy-policy')}
+                            sx={{
+                                fontSize: theme.typography.labelxs.fontSize,
+                                color: theme.palette.primary.main,
+                                cursor: 'pointer'
+                            }}
+                        >
+                            Terms & Conditions
+                        </Typography>
+                        <FiberManualRecord sx={{color: theme.palette.secondary.light, fontSize: '10px'}}/>
+                        <Typography onClick={()=>router.push('/privacy-policy')}
+                            sx={{
+                                fontSize: theme.typography.labelxs.fontSize,
+                                color: theme.palette.primary.main,
+                                cursor: 'pointer'
+                            }}
+                        >
+                            Privacy policy
+                        </Typography>
+                        <FiberManualRecord sx={{color: theme.palette.secondary.light, fontSize: '10px'}}/>
+                        <Typography
+                            onMouseEnter={()=>setType('about')}
+                            onClick={()=>setOpen(true)}
+                            sx={{
+                                fontSize: theme.typography.labelxs.fontSize,
+                                color: theme.palette.primary.main,
+                                cursor: 'pointer'
+                            }}
+                        >
+                            About us
+                        </Typography>
+                        <FiberManualRecord sx={{color: theme.palette.secondary.light, fontSize: '10px'}}/>
+                        <Typography
+                            onMouseEnter={()=>setType('contact')}
+                            onClick={()=>setOpen(true)}
+                            sx={{
+                                fontSize: theme.typography.labelxs.fontSize,
+                                color: theme.palette.primary.main,
+                                cursor: 'pointer'
+                            }}
+                        >
+                            Contact us
+                        </Typography>
+                    </Box>
                 </Box>
             </Box>
 
@@ -313,6 +315,6 @@ export default function Footer() {
                     </Box>
                 </Box>
             </MModal>
-        </Box>
+        </>
     )
 }
